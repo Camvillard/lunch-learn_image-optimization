539009936a32787edf02aae67f2e98a09438fe2d
camille <hello@camillevillard.com>
2022-03-03 10:12:14 -0500

    refactor(image): change image sources

--- src/shared/images/images.data.ts
+++ src/shared/images/images.data.ts
@@ -1,8 +1,8 @@
 export const imagePortrait =
-  "https://images.ctfassets.net/tacg73mas1jx/7BjJKFiBFGi9aAfJUb23Jw/9f3b6f685dfdbf232fc7e1e91fefe2ad/21_alberta-lake-louise-1.jpg";
+  "https://images.ctfassets.net/owdozk7lcobb/69GEoIZKX5LU8o3yPvJWRW/4503dd35075b9f4b6d9a47d5922b0623/21_alberta-lake-louise-1.jpg";
 
 export const imageLandscapeSmall =
-  "https://images.ctfassets.net/tacg73mas1jx/6JIgrp6LJWxsC6MbREqVcc/cea3469fbe96745b12ae84b6ed86e560/21_alberta-lake-louise-2_2.jpg";
+  "https://images.ctfassets.net/owdozk7lcobb/2RXiywHfx9PMI4cLZWBF40/754806a42118c7877c91ffbaaa421956/21_alberta-lake-louise-2_2.jpg";
 
 export const imageLandscapeBig =
-  "https://images.ctfassets.net/tacg73mas1jx/2iUxub9p6R12QSlr2MsGvT/4f0cc0031e911b08023cecea8cbf5e38/21_alberta-lake-louise-3.jpg";
+  "https://images.ctfassets.net/owdozk7lcobb/1Cay9cCKBtGQMWC3BcOgtp/0f2543ba980a4f950d80ce54a5b574a4/21_alberta-lake-louise-3.jpg";
